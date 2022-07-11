c47fc44fba04c29c0e5f74f27817391fc4f837d6
a.grebnev <artemgrebnev@gmail.com>
2022-07-11 10:15:17 +0500

    fix bug

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,8 @@ export class AppComponent implements OnInit {
   workPlaceOptions: string[] = ['1infinnity', '2infinnity', '3infinnity']
   // @ts-ignore
   filteredOptions: Observable<string[]>
+  formSendStatus : boolean = false
+
 
   profileForm = this.fb.group({
     firstName: ['', Validators.required],
@@ -29,7 +31,7 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    console.log(this.profileForm)
+    //console.log(this.profileForm)
 
     this.filteredOptions = this.profileForm.controls.workPlace.valueChanges.pipe(
       startWith(''),
@@ -39,6 +41,10 @@ export class AppComponent implements OnInit {
     );
   }
 
+  sendForm(): void {
+    this.formSendStatus = true
+  }
+
   logForm(): void {
     console.log(this.profileForm)
   }
@@ -46,7 +52,7 @@ export class AppComponent implements OnInit {
 
   private _filter(value: string): string[] {
     const filterValue = value.toLowerCase();
-    console.log(this.workPlaceOptions.filter(option => option.toLowerCase().includes(filterValue)))
+    //console.log(this.workPlaceOptions.filter(option => option.toLowerCase().includes(filterValue)))
     return this.workPlaceOptions.filter(option => option.toLowerCase().includes(filterValue));
   }
 }
